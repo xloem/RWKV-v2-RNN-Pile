4da0d37d70287ef95b5c094514d71b5042b14457
John Doe <johndoe@example.com>
2022-06-20 17:10:10 -0400

    tst fix

--- test.bash
+++ test.bash
@@ -4,7 +4,7 @@ do
     WEIGHTS="${WEIGHTS_FILE%.pth}"
     echo "CLASSIC"
     pushd RWKV-v2-RNN-Pile
-    python3 main.py --task lambada --model GPT_FULL/"$WEIGHTS"
+    python3 main.py --task lambada --model GPT_FULL/"$WEIGHTS" --device cuda --batch_size 16
     
     for model in GPT_RNN/"$WEIGHTS" GPT_FULL/"$WEIGHTS"
     do
