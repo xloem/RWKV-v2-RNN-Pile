5d1bd25cbe8bf13dcaeaaf81ee16629649d21937
John Doe <johndoe@example.com>
2022-06-20 17:20:28 -0400

    getting empty runs; dropped batch size

--- test.bash
+++ test.bash
@@ -4,7 +4,7 @@ do
     WEIGHTS="${WEIGHTS_FILE%.pth}"
     echo "CLASSIC"
     pushd RWKV-v2-RNN-Pile
-    python3 main.py --task lambada --model GPT_FULL/"$WEIGHTS" --device cuda --batch_size 32
+    python3 main.py --task lambada --model GPT_FULL/"$WEIGHTS" --device cuda --batch_size 16
     popd
     
     for model in GPT_RNN/"$WEIGHTS" GPT_FULL/"$WEIGHTS"
